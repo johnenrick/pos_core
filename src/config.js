2c02521c0430786f4be88b789f901012d1fff341
johnenrick <johnenrickplenos@gmail.com>
2018-07-23 20:06:51 +0800

    package json

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-let LIVE_BACKEND_URL = 'http://lite-bus-pos.dyndns.biz/api//api' // DEV
+let LIVE_BACKEND_URL = 'http://lite-bus-pos.dyndns.biz/api' // DEV
 // let LIVE_BACKEND_URL = 'https://api.ilinya.com/api' // PRODUCTION
 let DEV_BACKEND_URL = 'http://localhost/pos_core/api'
 let isDev = !(process.env.NODE_ENV === 'production') // Set to false in live
