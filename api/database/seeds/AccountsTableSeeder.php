bf68aab9aa1d7be9cc42c10edb629755152f1805
johnenrick <johnenrickplenos@gmail.com>
2018-07-23 14:49:30 +0800

    db seeder update

--- api/database/seeds/AccountsTableSeeder.php
+++ api/database/seeds/AccountsTableSeeder.php
@@ -20,11 +20,11 @@ class AccountsTableSeeder extends Seeder
         DB:: table('account_informations')->truncate();
         DB:: table('accounts') -> insert(array(
           array("id" => "1", "account_type_id" => 1, "username" => "Admin", "email" => "admin@gmail.com", "password" => $adminPassword),
-          array("id" => "2", "account_type_id" => 4, "username" => "Conductor", "email" => "conductor@gmail.com", "password" => Hash::make('Conductor')),
-          array("id" => "3", "account_type_id" => 3, "username" => "Driver", "email" => "driver@gmail.com", "password" => Hash::make('Driver')),
-          array("id" => "4", "account_type_id" => 5, "username" => "Cashier", "email" => "cashier@gmail.com", "password" => Hash::make('Cashier')),
-          array("id" => "5", "account_type_id" => 6,  "username" => "Cook", "email" => "cook@gmail.com", "password" => Hash::make('Cook')),
-          array("id" => "6", "account_type_id" => 7,  "username" => "Server", "email" => "server@gmail.com", "password" => Hash::make('Server')),
+          array("id" => "2", "account_type_id" => 4, "username" => "Conductor", "email" => "conductor@gmail.com", "password" => Hash::make('conductor')),
+          array("id" => "3", "account_type_id" => 3, "username" => "Driver", "email" => "driver@gmail.com", "password" => Hash::make('driver')),
+          array("id" => "4", "account_type_id" => 5, "username" => "Cashier", "email" => "cashier@gmail.com", "password" => Hash::make('cashier')),
+          array("id" => "5", "account_type_id" => 6,  "username" => "Cook", "email" => "cook@gmail.com", "password" => Hash::make('cook')),
+          array("id" => "6", "account_type_id" => 7,  "username" => "Server", "email" => "server@gmail.com", "password" => Hash::make('server')),
         ));
         DB:: table('account_informations') -> insert(array(
           array("id" => "1", "account_id" => 1, "first_name" => $faker->firstName, "middle_name" => $faker->lastName, "last_name" => $faker->lastName),
