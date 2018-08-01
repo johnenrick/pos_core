37d225a63c704baa7d36a9481c73d76df8fa641a
johnenrick <johnenrickplenos@gmail.com>
2018-08-02 02:21:09 +0800

    test

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -74,7 +74,7 @@ module.exports = {
         query: {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
-          publicPath: process.env.NODE_ENV === 'production' ? '../' : '/'
+          publicPath: process.env.NODE_ENV === 'production' ? '../../' : '/'
         }
       },
       {
