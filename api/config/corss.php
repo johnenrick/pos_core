cb9e5a0df45a3f1b1e3e7daf81533b368dc62509
johnenrick <johnenrickplenos@gmail.com>
2018-10-23 16:48:42 +0800

    Cors

---
+++ api/config/corss.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+
+    'supportsCredentials' => false,
+    'allowedOrigins' => [],
+    'allowedOriginsPatterns' => [],
+    'allowedHeaders' => ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'authorization', 'Token', 'token'],
+    'allowedMethods' => ['POST', 'GET', 'OPTIONS', 'DELETE', 'PUT'],
+    'exposedHeaders' => ['Token', 'token', 'Authorization', 'authorization'],
+    'maxAge' => 0,
+
+];
