664a2c55ca9f7317b0020285bdd7612a553f13eb
johnenrick <johnenrickplenos@gmail.com>
2018-10-23 17:08:53 +0800

    PIN

--- api/database/seeds/AccountsTableSeeder.php
+++ api/database/seeds/AccountsTableSeeder.php
@@ -19,29 +19,29 @@ class AccountsTableSeeder extends Seeder
         $employeePassword = Hash::make('employee');
         DB:: table('accounts')->truncate();
         DB:: table('accounts')-> insert(array(
-          array("id" => "1", "account_type_id" => 1, "username" => "Admin", "email" => "admin@gmail.com", "password" => $adminPassword),
-          array("id" => "2", "account_type_id" => 4, "username" => "Conductor", "email" => "conductor@gmail.com", "password" => Hash::make('conductor')),
-          array("id" => "3", "account_type_id" => 3, "username" => "Driver", "email" => "driver@gmail.com", "password" => Hash::make('driver')),
-          array("id" => "4", "account_type_id" => 5, "username" => "Cashier", "email" => "cashier@gmail.com", "password" => Hash::make('cashier')),
-          array("id" => "5", "account_type_id" => 6,  "username" => "Cook", "email" => "cook@gmail.com", "password" => Hash::make('cook')),
-          array("id" => "6", "account_type_id" => 7,  "username" => "Server", "email" => "server@gmail.com", "password" => Hash::make('server'))
+          array("id" => "1", "account_type_id" => 1, "username" => "Admin", "email" => "admin@gmail.com", "password" => $adminPassword, "pin" => 1234),
+          array("id" => "2", "account_type_id" => 4, "username" => "Conductor", "email" => "conductor@gmail.com", "password" => Hash::make('conductor'), "pin" => 1234),
+          array("id" => "3", "account_type_id" => 3, "username" => "Driver", "email" => "driver@gmail.com", "password" => Hash::make('driver'), "pin" => 1234),
+          array("id" => "4", "account_type_id" => 5, "username" => "Cashier", "email" => "cashier@gmail.com", "password" => Hash::make('cashier'), "pin" => 1234),
+          array("id" => "5", "account_type_id" => 6,  "username" => "Cook", "email" => "cook@gmail.com", "password" => Hash::make('cook'), "pin" => 1234),
+          array("id" => "6", "account_type_id" => 7,  "username" => "Server", "email" => "server@gmail.com", "password" => Hash::make('server'), "pin" => 1234)
         ));
         // mock conductor data
         for($id = 7; $id <= 57; $id++){
           DB:: table('accounts') -> insert(
-            array("id" => $id, "account_type_id" => 4, "username" => "Conductor$id", "email" => "conductor$id@gmail.com", "password" => Hash::make('conductor'))
+            array("id" => $id, "account_type_id" => 4, "username" => "Conductor$id", "email" => "conductor$id@gmail.com", "password" => Hash::make('conductor'), "pin" => 1234)
           );
         }
         // mock driver data
         for($id = 58; $id <= 108; $id++){
           DB:: table('accounts') -> insert(
-            array("id" => $id, "account_type_id" => 3, "username" => "Driver", "email" => "driver@gmail.com", "password" => Hash::make('driver'))
+            array("id" => $id, "account_type_id" => 3, "username" => "Driver", "email" => "driver@gmail.com", "password" => Hash::make('driver'), "pin" => 1234)
           );
         }
         // mock inspector data
         for($id = 109; $id <= 130; $id++){
           DB:: table('accounts') -> insert(
-            array("id" => $id, "account_type_id" => 8, "username" => "Inspector".$faker->firstName, "email" => $faker->email, "password" => Hash::make('inspector'))
+            array("id" => $id, "account_type_id" => 8, "username" => "Inspector".$faker->firstName, "email" => $faker->email, "password" => Hash::make('inspector'), "pin" => 1234)
           );
         }
 
